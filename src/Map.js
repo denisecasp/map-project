6d28adac91cfe3db141f25176adbc52dab6868d4
denisecasp <den.casperson@gmail.com>
2023-05-19 15:19:43 +0200

    Added styling to the navbar

--- src/Map.js
+++ src/Map.js
@@ -1,4 +1,6 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+
 import Typography from "@mui/material/Typography";
 import TextField  from "@mui/material/TextField";
 import Slider from "@mui/material/Slider";
@@ -11,7 +13,7 @@ export default class Map extends React.Component {
 
     header = () =>{
         return(
-            <div style={{backgroundColor: "lightblue"}}>
+            <div>
             <Typography variant="h4" style={{ textAlign: "center" }}>
                 L U N C H 
             </Typography>
@@ -20,34 +22,44 @@ export default class Map extends React.Component {
                 <Typography>
                     Avstånd:
                 </Typography>
-               <Slider style={{ widht: "80%" }} /> 
+               <Slider style={{ width: "85%" }} /> 
             </div>
             
-            <div>
-                <Button variant="outlined"
-                    style={{ width: "50%" }}
-                >   
-                    <RestartAltIcon />
-                    Återställ</Button>
-                <Button variant="contained"
-                    style={{ width: "50%" }}
-                >
+            <div>  <Button variant="contained"
+                    style={{ width: "50%" }}>
                     <SearchIcon />
-                    Sök</Button>
+                    Sök
+                    </Button>
+                    <Button variant="outlined"
+                    style={{ width: "50%" }}>   
+                    <RestartAltIcon />
+                    Återställ
+                    </Button>
+              
             </div>
         </div>
         )
     }
     map = () =>{
         return(
-            <div style={{backgroundColor: "skyblue"}}>
-            Map
+            <div style={{backgroundColor: "pink" }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyBYrV501SwGTqexub8U6JbqIJ44cxGx5I4" }}
+                defaultCenter={{
+                    lat: 10.99835602,
+                    lng: 77.01502627
+                }}
+                defaultZoom={14}
+            >
+       
+
+            </GoogleMapReact>
         </div>
         )
     }
     render() {
         return (
-            <div>
+            <div style={{backgroundColor: "skyblue"}}>
                     {this.header()}
                     {this.map()}
             </div>
