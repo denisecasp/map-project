9a62139c96df3a23de478a8e99748ccf8c0bf514
denisecasp <den.casperson@gmail.com>
2023-05-19 13:42:38 +0200

    Cleaned the code and added componetns Map and Rest

---
+++ src/Restaurants.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Restaurants extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    Restaurants
+                </h1>
+            </div>
+        )
+    }
+}
