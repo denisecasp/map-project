4b76ec2f0bce66474214411935e3daa5a83871a9
denisecasp <den.casperson@gmail.com>
2023-05-30 17:33:16 +0200

    added the current location icon

--- src/Map.js
+++ src/Map.js
@@ -8,7 +8,7 @@ import Button from "@mui/material/Button";
 
 import SearchIcon from '@mui/icons-material/Search';
 import RestartAltIcon from '@mui/icons-material/RestartAlt';
-
+import PersonPinIcon from '@mui/icons-material/PersonPin';
 
 
 export default class Map extends React.Component {
@@ -16,7 +16,7 @@ export default class Map extends React.Component {
     
 
     constructor(props) {
-       super();
+        super();
         this.state = {
             latitude: 24.723456,
             longitude: 46.70095
@@ -72,15 +72,18 @@ export default class Map extends React.Component {
             <div style={{backgroundColor: "pink", height: "80vh"}}>
             <GoogleMapReact
                 bootstrapURLKeys={{ key: "AIzaSyBYrV501SwGTqexub8U6JbqIJ44cxGx5I4" }}
-                defaultCenter={{
-                    lat: 10.99835602,
-                    lng: 77.01502627
+               
+                defaultZoom={16}
+                center={{ 
+                    lat: this.state.latitude,
+                    lng: this.state.longitude 
                 }}
-                defaultZoom={12}
-                center={{ lat: this.state.latitude, lng: this.state.longitude }}
             >
        
-
+                <PersonPinIcon color={"primary"}
+                    lat={this.state.latitude}
+                    lng={this.state.longitude}
+                />
             </GoogleMapReact>
         </div>
         )
